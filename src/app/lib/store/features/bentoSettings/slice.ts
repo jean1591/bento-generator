51590065584cc7cb1ae0171a261c6099de0221ed
Jean Robertou <j-rob@live.fr>
2024-05-31 22:16:18 +0200

    handle same col/same row merge

--- src/app/lib/store/features/bentoSettings/slice.ts
+++ src/app/lib/store/features/bentoSettings/slice.ts
@@ -1,4 +1,4 @@
-import { Bento, generateEmptyBento, generateRandomBento } from "@/utils";
+import { Bento, generateEmptyBento } from "@/utils";
 
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
@@ -10,46 +10,22 @@ export interface CellCoordinates {
 
 export interface BentoSettingsState {
   bento: Bento;
-  columnNumber: number;
-  displayCopyToast: boolean;
-  emptyBento: Bento;
-  rowNumber: number;
   selectedCellOne: CellCoordinates | null;
   selectedCellTwo: CellCoordinates | null;
 }
 
 const initialState: BentoSettingsState = {
-  bento: generateRandomBento(6, 5),
-  columnNumber: 6,
-  displayCopyToast: false,
-  emptyBento: generateEmptyBento(3, 3),
-  rowNumber: 5,
+  bento: generateEmptyBento(3, 3),
   selectedCellOne: null,
-  selectedCellTwo: null
+  selectedCellTwo: null,
 };
 
 export const bentoSettingsSlice = createSlice({
   name: "bentoSettings",
   initialState,
   reducers: {
-    setBento: (state) => {
-      state.bento = generateRandomBento(state.columnNumber, state.rowNumber);
-      state.displayCopyToast = false;
-    },
-    setColumnNumber: (state, action: PayloadAction<number>) => {
-      state.columnNumber = action.payload;
-      state.bento = generateRandomBento(state.columnNumber, state.rowNumber);
-      state.emptyBento = generateEmptyBento(state.columnNumber, state.rowNumber);
-      state.displayCopyToast = false;
-    },
-    setRowNumber: (state, action: PayloadAction<number>) => {
-      state.rowNumber = action.payload;
-      state.bento = generateRandomBento(state.columnNumber, state.rowNumber);
-      state.emptyBento = generateEmptyBento(state.columnNumber, state.rowNumber);
-      state.displayCopyToast = false;
-    },
-    setDisplayCopyToast: (state, action: PayloadAction<boolean>) => {
-      state.displayCopyToast = action.payload;
+    setBento: (state, action: PayloadAction<Bento>) => {
+      state.bento = action.payload;
     },
     setSelectedCellOne: (state, action: PayloadAction<CellCoordinates>) => {
       state.selectedCellOne = action.payload;
@@ -60,8 +36,7 @@ export const bentoSettingsSlice = createSlice({
   },
 });
 
-export const { setBento, setDisplayCopyToast, setColumnNumber, setRowNumber, setSelectedCellOne,
-  setSelectedCellTwo } =
+export const { setBento, setSelectedCellOne, setSelectedCellTwo } =
   bentoSettingsSlice.actions;
 
 export default bentoSettingsSlice.reducer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { Bento, classNames } from "@/utils";
+import {
+  CellCoordinates,
+  setBento,
+} from "./lib/store/features/bentoSettings/slice";
+import { useDispatch, useSelector } from "react-redux";
+
+import { RootState } from "./lib/store/store";
+import { cloneDeep } from "lodash";
 import { useState } from "react";
-import { CellCoordinates } from "./lib/store/features/bentoSettings/slice";
-import { Bento, classNames, generateEmptyBento } from "@/utils";
 
 export default function Home() {
-  const [col, setCol] = useState<number>(3);
-  const [row, setRow] = useState<number>(3);
-
-  const [bento, setBento] = useState<Bento>(generateEmptyBento(col, row));
+  const dispatch = useDispatch();
+  const bento = useSelector((state: RootState) => state.bentoSettings.bento);
 
   const [cellOne, setCellOne] = useState<CellCoordinates | null>(null);
   const [cellTwo, setCellTwo] = useState<CellCoordinates | null>(null);
@@ -16,18 +21,6 @@ export default function Home() {
   const handleCellOnClick = (rowIndex: number, columnIndex: number) => {
     const isSelectedCellOne = isCellCurrentCell(cellOne, rowIndex, columnIndex);
     const isSelectedCellTwo = isCellCurrentCell(cellTwo, rowIndex, columnIndex);
-
-    console.log({
-      cellOne,
-      cellTwo,
-      isSelectedCellOne,
-      isSelectedCellTwo,
-      "cellOne && isSelectedCellOne": cellOne && isSelectedCellOne,
-      "cellTwo && isSelectedCellTwo": cellTwo && isSelectedCellTwo,
-      "(!cellOne && cellTwo) || (!cellOne && !cellTwo)":
-        (!cellOne && cellTwo) || (!cellOne && !cellTwo),
-      "cellOne && !cellTwo": cellOne && !cellTwo,
-    });
 
     // Cancel selectedCellOne
     if (cellOne && isSelectedCellOne) {
@@ -60,21 +53,47 @@ export default function Home() {
     }
 
     // Find most up left cell
-    let cellToUpdate = cellOne
-    let cellToCheck = cellTwo
-    
-    if (cellOne?.columnIndex**2 + cellOne?.rowIndex**2 > cellTwo?.columnIndex**2 + cellTwo?.rowIndex**2) {
-      cellToUpdate = cellTwo
-      cellToCheck = cellOne
+    let cellToUpdate = cellOne;
+    let cellToHide = cellTwo;
+
+    if (
+      cellOne?.columnIndex ** 2 + cellOne?.rowIndex ** 2 >
+      cellTwo?.columnIndex ** 2 + cellTwo?.rowIndex ** 2
+    ) {
+      cellToUpdate = cellTwo;
+      cellToHide = cellOne;
     }
 
-    // Check if other cell is neighbour (vertical & horizontal)
-    if (cellToUpdate.rowIndex === cellToCheck.rowIndex || cellToUpdate.columnIndex === cellToCheck.columnIndex) {
-      // Update up left cell col-span / row-span
-      // ????? HOW TO MERGE ?????
+    // TODO: check cellToUpdate.row/columIndex +/- 1 cellToCheck.row/columIndex
+    // Update horizontal
+    if (cellToUpdate.rowIndex === cellToHide.rowIndex) {
+      const updatedBento = cloneDeep(bento);
+
+      const currentCellLength =
+        bento[cellToUpdate.rowIndex][cellToUpdate.columnIndex][0];
+
+      updatedBento[cellToUpdate.rowIndex][cellToUpdate.columnIndex] = [
+        currentCellLength + 1,
+        1,
+      ];
+      updatedBento[cellToHide.rowIndex][cellToHide.columnIndex] = [0, 0];
+
+      dispatch(setBento(updatedBento));
+    }
+    // Update vertical
+    else if (cellToUpdate.columnIndex === cellToHide.columnIndex) {
+      const updatedBento = cloneDeep(bento);
+
+      updatedBento[cellToUpdate.rowIndex][cellToUpdate.columnIndex] = [1, 2];
+      updatedBento[cellToHide.rowIndex][cellToHide.columnIndex] = [0, 0];
+
+      dispatch(setBento(updatedBento));
     }
 
-    return 
+    setCellOne(null);
+    setCellTwo(null);
+
+    return;
   };
 
   return (
@@ -91,30 +110,19 @@ export default function Home() {
         </button>
       </div>
 
-      <div
-        className={classNames(
-          col === 1 ? "grid-cols-1" : "",
-          col === 2 ? "grid-cols-2" : "",
-          col === 3 ? "grid-cols-3" : "",
-          col === 4 ? "grid-cols-4" : "",
-          col === 5 ? "grid-cols-5" : "",
-          "mt-20 grid gap-5"
-        )}
-      >
+      <div className="mt-20 grid grid-cols-3 gap-5">
         {bento.map((row, rowIndex) =>
           row.map((size, columnIndex) => {
             if (size[0] !== 0 && size[1] !== 0) {
               return (
                 <div
                   key={[rowIndex, columnIndex].toString()}
-                  className={classNames(
-                    isCellCurrentCell(cellOne, rowIndex, columnIndex)
-                      ? "border-red-100 bg-red-200 shadow-2xl"
-                      : "border-blue-100 bg-blue-200 shadow-sm",
-                    isCellCurrentCell(cellTwo, rowIndex, columnIndex)
-                      ? "border-green-100 bg-green-200 shadow-2xl"
-                      : "border-blue-100 bg-blue-200 shadow-sm",
-                    "border h-32 transition ease-in-out duration-500 rounded-lg"
+                  className={classNamesGenerator(
+                    size,
+                    cellOne,
+                    cellTwo,
+                    rowIndex,
+                    columnIndex
                   )}
                   onClick={() => handleCellOnClick(rowIndex, columnIndex)}
                 >
@@ -140,5 +148,29 @@ const isCellCurrentCell = (
     firstCell !== null &&
     firstCell.columnIndex === columnIndex &&
     firstCell.rowIndex === rowIndex
+  );
+};
+
+const classNamesGenerator = (
+  size: number[],
+  cellOne: CellCoordinates | null,
+  cellTwo: CellCoordinates | null,
+  rowIndex: number,
+  columnIndex: number
+) => {
+  return classNames(
+    // size[0] is column span
+    size[0] === 3 ? "col-span-3" : "",
+    size[0] === 2 ? "col-span-2" : "",
+    // size[1] is row span
+    size[1] === 3 ? "row-span-3" : "",
+    size[1] === 2 ? "row-span-2" : "",
+    isCellCurrentCell(cellOne, rowIndex, columnIndex)
+      ? "border-red-100 bg-red-200 shadow-2xl"
+      : "",
+    isCellCurrentCell(cellTwo, rowIndex, columnIndex)
+      ? "border-green-100 bg-green-200 shadow-2xl"
+      : "",
+    "border min-h-32 transition ease-in-out duration-500 rounded-lg border-blue-100 bg-blue-200 shadow-sm"
   );
 };
