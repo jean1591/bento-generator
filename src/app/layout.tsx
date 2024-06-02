67008fee1de8552e9df556cb7b848c7a9aff960a
Jean Robertou <j-rob@live.fr>
2024-06-02 17:24:16 +0200

    delete next-theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { StoreProvider } from "./lib/store/storeProvider";
 import { classNames } from "@/utils";
-import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
@@ -14,15 +13,10 @@ export default function RootLayout({
   return (
     <StoreProvider>
       <html lang="fr" suppressHydrationWarning>
-        <body
-          className={classNames(
-            inter.className,
-            "bg-slate-200"
-          )}
-        >
-            <div className="py-8 mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 mb-16">
-              {children}
-            </div>
+        <body className={classNames(inter.className, "bg-slate-200")}>
+          <div className="py-8 mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 mb-16">
+            {children}
+          </div>
         </body>
       </html>
     </StoreProvider>
