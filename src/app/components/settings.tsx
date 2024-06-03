d13c9f41787e6b7fa3ebd8dfe46abb5f4fc06191
Jean Robertou <j-rob@live.fr>
2024-06-03 07:38:58 +0200

    copy code and display toast

--- src/app/components/settings.tsx
+++ src/app/components/settings.tsx
@@ -6,13 +6,18 @@ import {
 } from "@/utils";
 import {
   setBento,
+  setDisplayToast,
+  setErrorMessage,
   setSelectedCellOne,
   setSelectedCellTwo,
 } from "../lib/store/features/bentoSettings/slice";
 import { useDispatch, useSelector } from "react-redux";
 
+import { Bento } from "./bento";
 import { Button } from "./button";
 import { RootState } from "../lib/store/store";
+import { StoreProvider } from "../lib/store/storeProvider";
+import { renderToStaticMarkup } from "react-dom/server";
 
 export const Settings = () => {
   const dispatch = useDispatch();
@@ -30,6 +35,23 @@ export const Settings = () => {
     selectedCellOne,
     selectedCellTwo
   );
+
+  const handleOnCopy = () => {
+    dispatch(setErrorMessage("Code successfully copied !"));
+    dispatch(setDisplayToast(true));
+
+    navigator.clipboard.writeText(
+      renderToStaticMarkup(
+        <StoreProvider>
+          <Bento />
+        </StoreProvider>
+      )
+    );
+
+    setTimeout(() => {
+      dispatch(setDisplayToast(false));
+    }, 5000);
+  };
 
   const handleMerge = () => {
     if (!selectedCellOne && !selectedCellTwo) {
@@ -63,11 +85,19 @@ export const Settings = () => {
         <SetRowsNumber />
       </div>
 
-      <Button
-        label={canRestoreCells ? "Restore cells" : "Merge cells"}
-        buttonDisabled={mergeButtonDisable}
-        onClick={handleMerge}
-      />
+      <div className="flex items-center justify-end gap-x-10">
+        <Button
+          label={canRestoreCells ? "Restore cells" : "Merge cells"}
+          buttonDisabled={mergeButtonDisable}
+          onClick={handleMerge}
+        />
+
+        <Button
+          label={"Get the code"}
+          buttonDisabled={false}
+          onClick={handleOnCopy}
+        />
+      </div>
     </div>
   );
 };
